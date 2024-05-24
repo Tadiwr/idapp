afd21b7692f16fdd468ce5964a3ec18494dbf3b4
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-05-24 07:51:39 +0200

    initial commit

---
+++ backend/idapp/src/main/java/com/codapt/idapp/IdappApplication.java
@@ -0,0 +1,13 @@
+package com.codapt.idapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdappApplication.class, args);
+	}
+
+}
