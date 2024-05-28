926b059c7b7c946cac2716e12105da8ac4703549
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-05-28 19:48:47 +0200

    adds hex util to code generation

--- backend/idapp/src/main/java/com/codapt/idapp/features/shared/generators/bookingcodes/BookingCodeGenetorImpl.java
+++ backend/idapp/src/main/java/com/codapt/idapp/features/shared/generators/bookingcodes/BookingCodeGenetorImpl.java
@@ -3,6 +3,7 @@ package com.codapt.idapp.features.shared.generators.bookingcodes;
 import org.springframework.stereotype.Service;
 
 import com.codapt.idapp.features.bookings.Booking;
+import com.codapt.idapp.features.shared.utils.HexUtil;
 
 @Service
 public class BookingCodeGenetorImpl implements BookingCodeGenerator {
@@ -13,9 +14,9 @@ public class BookingCodeGenetorImpl implements BookingCodeGenerator {
         String day = "" + booking.getDate().getDayOfMonth();
         String month = "" + booking.getDate().getMonthValue();
         String year = "" + booking.getDate().getYear();
+        String countHex = new HexUtil().toHex(count);
         
-
-        return cityCode + "." + day + month + year + "." +count;
+        return cityCode + "-" + day + month + year + "-" + countHex;
     }
     
 }
