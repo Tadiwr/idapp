df841b1953b9df02d7e25eb16212b44cf0def893
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-05-29 10:51:52 +0200

    adds M-1 relationship with office

--- backend/idapp/src/main/java/com/codapt/idapp/features/bookings/BookingsService.java
+++ backend/idapp/src/main/java/com/codapt/idapp/features/bookings/BookingsService.java
@@ -3,10 +3,14 @@ package com.codapt.idapp.features.bookings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.codapt.idapp.features.offices.Office;
+import com.codapt.idapp.features.offices.OfficeService;
 import com.codapt.idapp.features.shared.dto.BookingDayDTO;
 import com.codapt.idapp.features.shared.dto.BookingsDTO;
 import com.codapt.idapp.features.shared.generators.bookingcodes.BookingCodeGenerator;
 import com.codapt.idapp.features.shared.schedulers.FisrtComeFirstServe;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class BookingsService {
@@ -20,15 +24,21 @@ public class BookingsService {
     @Autowired
     BookingCodeGenerator codeGenerator;
 
+    @Autowired
+    OfficeService officeService;
+
+    @Transactional
     public Booking make(BookingsDTO booking) {
 
         Booking newBooking = new Booking();
         BookingDayDTO bookingDay = schedueler.generateDay();
+
+        Office office = officeService.getOfficeById(booking.getOfficeId()).get();
         
         newBooking.setFirstName(booking.getFirstName());
         newBooking.setLastName(booking.getLastName());
         newBooking.setPhoneNumber(booking.getPhoneNumber());
-        newBooking.setOfficeId(booking.getOfficeId());
+        newBooking.setOffice(office);
         newBooking.setDate(bookingDay.getDate());
 
         String bookingCode = codeGenerator.generateCode(newBooking, bookingDay.getCount());
