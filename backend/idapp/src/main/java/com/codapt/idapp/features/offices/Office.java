b47aea9e1e0d9733c9af828f2ceba53cce8230ea
= <=>
2024-05-29 10:54:29 +0200

    adds relationship with bookings model

--- backend/idapp/src/main/java/com/codapt/idapp/features/offices/Office.java
+++ backend/idapp/src/main/java/com/codapt/idapp/features/offices/Office.java
@@ -1,10 +1,16 @@
 package com.codapt.idapp.features.offices;
 
+import java.util.List;
+
+import com.codapt.idapp.features.bookings.Booking;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -23,5 +29,9 @@ public class Office {
 
     @Column
     private String shortName;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "office")
+    private List<Booking> bookings;
 
 }
