8f96b5596e8cdecc8fe9a1265ec2047dfb5d339b
= <=>
2024-07-08 14:18:23 +0200

    simplified dir

--- server/src/main/java/com/codapt/idapp/features/bookings/BookingController.java
+++ server/src/main/java/com/codapt/idapp/features/bookings/BookingController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -59,6 +58,12 @@ public class BookingController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @PostMapping("/setregistered")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
     
 
 }
