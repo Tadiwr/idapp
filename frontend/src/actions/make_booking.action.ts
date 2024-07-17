74b6127f314620f5d5f76ca7e3683cab14a9b786
= <=>
2024-07-17 09:09:53 +0200

    adds make booking funciton

---
+++ frontend/src/actions/make_booking.action.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { makeBooking } from "@/data/bookings.data";
+import { MakeBookingDTO } from "@/data/types";
+import { redirect } from "next/navigation";
+
+export default async function makeBookingAction(prevState : {message: string}, formData : FormData) {
+
+    // Extract details from form data
+    const details : MakeBookingDTO = extractDetails(formData);
+    let bookingCode : string = "";
+    // api call to server
+
+    try {
+        const res = await makeBooking(details);
+        bookingCode = res.code;
+    } catch (error) {
+        console.log(error)
+        return {message : "Try Again"}
+    }
+
+    redirect(`/book/confirm/${bookingCode}`)
+
+}
+
+function extractDetails(formData : FormData) : MakeBookingDTO {
+
+    const officeId : number = Number.parseInt(formData.get("office_id")?.toString() ?? "-1")
+
+    return {
+        officeId : officeId,
+        firstName : formData.get("first_name")?.toString() ?? "",
+        lastName : formData.get("office_id")?.toString() ?? "",
+        phoneNumber : formData.get("phone_number")?.toString() ?? "",
+    }
+}
