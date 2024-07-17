45786b6edec60d3b9c6e7e2f14a0c470433d6685
Tavonga-Mushohwe <tavonga.dev@gmail.com>
2024-07-17 09:26:16 +0200

    Made card clickable

--- frontend/src/components/info_card.tsx
+++ frontend/src/components/info_card.tsx
@@ -1,18 +1,22 @@
-export type InfoCardProps = {
-    title : string,
-    description : string,
-    href: string
-}
-  
-  
+import Link from "next/link"
 
-export function InfoCard({title, description, href} : InfoCardProps) {
-    return <>
-      <div className="flex p-5 border flex-col rounded-xl" > 
-          <h1 className="font-bold m-3" >{title}</h1>
-          <p className="text-slate-700 m-3" >{description}</p>
-          <a href={href} className="text-blue-400 m-3 font-medium hover:text-blue-700" >Take Me There</a>
+export type InfoCardProps = {
+  title: string,
+  description: string,
+  href: string
+}
+
+
+
+export function InfoCard({ title, description, href }: InfoCardProps) {
+  return <>
+      <div className="flex p-6 border flex-col rounded-xl hover:bg-blue-700 text-white transition-colors ease-in-out translate-y-5 duration-300 " >
+  <a href={href} className="p-3">
+        <h1 className="font-bold m-3 text-black" >{title}</h1>
+        <p className="text-slate-700 m-3" >{description}</p>
+        <p  className="text-blue-400 m-3 font-medium hover:text-white" >Take Me There</p>
+  </a>
+    
       </div>
-    </>
-  }
-  
+  </>
+}
