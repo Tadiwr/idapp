0b727ab2e51899ee90948ed69a4bbe8e516487cf
= <=>
2024-07-17 08:20:09 +0200

    adds checked state on sidebar

--- frontend/src/components/booking_side_bar.tsx
+++ frontend/src/components/booking_side_bar.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import React from 'react'
 import CheckBox from './check_box'
+import { usePathname } from 'next/navigation';
 
 type Props = {
     officeFlag: boolean,
@@ -8,22 +11,42 @@ type Props = {
 }
 
 export default function BookingSideBar({officeFlag, detailsFlag, confirmFlag} : Props) {
+
+
+    const pathName = usePathname();
+
+    const shouldCheck = (tabName : "details" | "select_office" | "confirm") => {
+        if (pathName == "/book") {
+            if (tabName == "select_office") {
+                return true
+            }
+        }
+
+        if (pathName.startsWith("/book/enter_info")) {
+            if (tabName == "details" || tabName == "select_office") {
+                return true
+            }
+        }
+
+        return false
+    }
+
   return (
     <div className='flex p-10 border-r flex-col w-full h-full' >
         <h1 className='font-bold text-4xl mb-10' >ID Book App</h1>
 
         <a href='/book' className='flex flex-row my-2 justify-start items-center' >
-            <CheckBox check={officeFlag} />
+            <CheckBox check={shouldCheck("select_office")} />
             <p className='font-medium text-lg' >Select Office</p>
         </a>
 
-        <a href='/book/enter_info' className='flex flex-row my-2 justify-start items-center' >
-            <CheckBox check={detailsFlag} />
+        <a className='flex flex-row my-2 justify-start items-center' >
+            <CheckBox check={shouldCheck("details")} />
             <h1 className='font-medium text-lg' >Enter Details</h1>
         </a>
 
         <a className='flex flex-row my-2 justify-start items-center' >
-            <CheckBox check={confirmFlag} />
+            <CheckBox check={shouldCheck("confirm")} />
             <h1 className='font-medium text-lg' >Confirm Booking</h1>
         </a>
 
