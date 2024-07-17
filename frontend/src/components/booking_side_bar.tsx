a85cfa48359e55d53ecebcb9b37e4d61870a2b3c
Tavonga-Mushohwe <tavonga.dev@gmail.com>
2024-07-17 14:07:11 +0200

    Fixed checkbox highlighting

--- frontend/src/components/booking_side_bar.tsx
+++ frontend/src/components/booking_side_bar.tsx
@@ -6,16 +6,16 @@ import { usePathname } from 'next/navigation';
 
 type Props = {
     officeFlag: boolean,
-    detailsFlag : boolean,
-    confirmFlag : boolean
+    detailsFlag: boolean,
+    confirmFlag: boolean
 }
 
-export default function BookingSideBar({officeFlag, detailsFlag, confirmFlag} : Props) {
+export default function BookingSideBar({ officeFlag, detailsFlag, confirmFlag }: Props) {
 
 
     const pathName = usePathname();
 
-    const shouldCheck = (tabName : "details" | "select_office" | "confirm") => {
+    const shouldCheck = (tabName: "details" | "select_office" | "confirm") => {
         if (pathName == "/book") {
             if (tabName == "select_office") {
                 return true
@@ -27,32 +27,38 @@ export default function BookingSideBar({officeFlag, detailsFlag, confirmFlag} : 
                 return true
             }
         }
+        if (pathName.startsWith("/book/confirm/")) {
+            if (tabName == "confirm"|| tabName == "details" || tabName == "select_office") {
+                return true
+
+            }
+        }
 
         return false
     }
 
-  return (
-    <div className='flex p-10 border-r flex-col w-full h-full' >
-        <h1 className='font-bold text-4xl mb-10' >ID Book App</h1>
+    return (
+        <div className='flex p-10 border-r flex-col w-full h-full' >
+            <h1 className='font-bold text-4xl mb-10' >ID Book App</h1>
 
-        <a href='/book' className='flex flex-row my-2 justify-start items-center' >
-            <CheckBox check={shouldCheck("select_office")} />
-            <p className='font-medium text-lg' >Select Office</p>
-        </a>
+            <a href='/book' className='flex flex-row my-2 justify-start items-center' >
+                <CheckBox check={shouldCheck("select_office")} />
+                <p className='font-medium text-lg' >Select Office</p>
+            </a>
 
-        <a className='flex flex-row my-2 justify-start items-center' >
-            <CheckBox check={shouldCheck("details")} />
-            <h1 className='font-medium text-lg' >Enter Details</h1>
-        </a>
+            <a className='flex flex-row my-2 justify-start items-center' >
+                <CheckBox check={shouldCheck("details")} />
+                <h1 className='font-medium text-lg' >Enter Details</h1>
+            </a>
 
-        <a className='flex flex-row my-2 justify-start items-center' >
-            <CheckBox check={shouldCheck("confirm")} />
-            <h1 className='font-medium text-lg' >Confirm Booking</h1>
-        </a>
+            <a className='flex flex-row my-2 justify-start items-center' >
+                <CheckBox check={shouldCheck("confirm")} />
+                <h1 className='font-medium text-lg' >Confirm Booking</h1>
+            </a>
 
-        
-        
-    
-    </div>
-  )
+
+
+
+        </div>
+    )
 }
--- frontend/src/components/info_card.tsx
+++ frontend/src/components/info_card.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link"
 
 export type InfoCardProps = {
   title: string,
@@ -10,13 +9,13 @@ export type InfoCardProps = {
 
 export function InfoCard({ title, description, href }: InfoCardProps) {
   return <>
-      <div className="flex p-6 border flex-col rounded-xl hover:bg-blue-700 text-white transition-colors ease-in-out translate-y-5 duration-300 " >
-  <a href={href} className="p-3">
+    <div className="flex p-6 border flex-col rounded-xl hover:bg-blue-700 text-white transition-colors ease-in-out translate-y-5 duration-300 " >
+      <a href={href} className="p-3">
         <h1 className="font-bold m-3 text-black" >{title}</h1>
         <p className="text-slate-700 m-3" >{description}</p>
-        <p  className="text-blue-400 m-3 font-medium hover:text-white" >Take Me There</p>
-  </a>
-    
-      </div>
+        <p className="text-blue-400 m-3 font-medium hover:text-white" >Take Me There</p>
+      </a>
+
+    </div>
   </>
 }
